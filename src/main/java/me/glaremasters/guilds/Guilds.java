237cab139c249da38c1f56ca7f31226a16ba285e
darbyjack <admin@glaremasters.me>
2018-04-11 18:20:54 -0500

    Added in spigot updater

--- src/main/java/me/glaremasters/guilds/Guilds.java
+++ src/main/java/me/glaremasters/guilds/Guilds.java
@@ -1,6 +1,7 @@
 package me.glaremasters.guilds;
 
 import lombok.Getter;
+import me.glaremasters.guilds.utils.SpigotUpdater;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Guilds extends JavaPlugin {
@@ -9,6 +10,20 @@ public final class Guilds extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Check to see if there's any updates for the plugin
+        SpigotUpdater updater = new SpigotUpdater(this, 48920);
+        try {
+            // If there's an update, tell the user that they can update
+            if (updater.checkForUpdates()) {
+                getLogger().info("You appear to be running a version other than our latest stable release."
+                        + " You can download our newest version at: " + updater
+                        .getResourceURL());
+            }
+        } catch (Exception e) {
+            // If it can't check for an update, tell the user and throw an error.
+            getLogger().info("Could not check for updates! Stacktrace:");
+            e.printStackTrace();
+        }
 
     }
 
